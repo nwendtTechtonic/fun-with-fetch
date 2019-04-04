adeac3cf23c84e79dd335d699759ff80828ed8e8
Nathaniel Wendt <nathaniel.wendt@techtonic.com>
2019-04-04 14:58:52 -0600

    feat: add self-invoking fetch request

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,30 @@
 const baseUrl = 'https://virtserver.swaggerhub.com/bgoers/comment-board/1.0.0';
-document.getElementById('btn').addEventListener('click', fetchComments)
+document.getElementById('fetch-btn').addEventListener('click', fetchComments)
+
+// (function fetchComments(event) {
+//   fetch(`https://us-central1-fir-cb-backend.cloudfunctions.net/api/comment/`)
+//   .then(res => res.json())
+//   // .then(data => console.log(data))
+//   .then(data => appendHTML(data));
+//
+//   setTimeout(fetchComments, 5000)
+// })();
 
 function fetchComments(event) {
-
   fetch(`https://us-central1-fir-cb-backend.cloudfunctions.net/api/comment/`)
   .then(res => res.json())
-  // .then(data => console.log(data))
   .then(data => appendHTML(data));
 }
-
 
 function appendHTML(data) {
   console.log(data);
   // console.log(data.length);
 }
+
+// create feature to post Comments
+
+// create function to find and display comments from specific author
+
+// create function to delete all comments from a specific author
+
+// create function to loop through all comments from specific author in search of a keyword
