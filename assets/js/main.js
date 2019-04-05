5a41114fd21d474bd0aa6023a7e76a9882f150ce
Nathaniel Wendt <nathaniel.wendt@techtonic.com>
2019-04-05 14:40:04 -0600

    feat: add deleteComment function to complete assignment requirements

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+fetchURL = `https://us-central1-fir-cb-backend.cloudfunctions.net/api/comment/`
 document.getElementById('fetch-btn').addEventListener('click', fetchComments);
 document.getElementById('submit-comment').addEventListener('click', postComment);
 
@@ -11,18 +12,26 @@ document.getElementById('submit-comment').addEventListener('click', postComment)
 // })();
 
 function fetchComments(event) {
-  fetch(`https://us-central1-fir-cb-backend.cloudfunctions.net/api/comment/`)
+  fetch(fetchURL)
   .then(res => res.json())
   .then(data => appendComments(data));
 }
 
 function appendComments(data) {
   let container = document.getElementById('comment-container');
-
-  for (var i = 0; i < data.length; i++) {
+  container.innerHTML = '';
+  for (let i = 0; i < data.length; i++) {
     let li = document.createElement('li');
+    let deleteBtn = document.createElement('button');
+      deleteBtn.innerText = 'delete';
+
     li.innerText = data[i].comment;
     container.append(li);
+    container.append(deleteBtn);
+
+    deleteBtn.addEventListener('click', function(){
+      deleteComment(data[i]._id);
+    })
   }
   isolateName(data);
 }
@@ -37,7 +46,7 @@ function postComment(event) {
 
   resetTextField = () => document.getElementById('comment').value = '';
 
-  fetch('https://us-central1-fir-cb-backend.cloudfunctions.net/api/comment/', {
+  fetch(fetchURL, {
     method: 'POST',
     body: JSON.stringify(data),
     headers: {'Content-Type': 'application/json'}
@@ -55,5 +64,16 @@ function isolateName(data) {
 }
 
 // create function to delete all comments from a specific author
+function deleteComment(id) {
+  return fetch(`${fetchURL}${id}`, {
+    method: 'DELETE'
+  }).then(function(response){
+    fetchComments();
+  });
+}
 
-// create function to loop through all comments from specific author in search of a keyword
+// create function to display a grid
+
+// create a function to search every comment for specific keywords "A1" "B1" etc.
+
+// create a function that hides/shows a class
